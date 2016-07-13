a05a444b8062050dc9355f6d798ea8925c9bed0a
Becca Nelson <beccanelson88@gmail.com>
2016-07-13 11:10:28 -0500

    Initial Commit

---
+++ iOS-tic-tac-toe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS-tic-tac-toe
+//
+//  Created by Becca Nelson on 7/13/16.
+//  Copyright © 2016 8th Light. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
